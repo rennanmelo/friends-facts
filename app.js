ca27dccb44c2fa6835600f1b59a1c3672f4c41d9
Rennan Melo <rennan.melo11@gmail.com>
2021-05-27 20:59:09 -0300

    Created first react component

---
+++ app.js
@@ -0,0 +1,13 @@
+(function() {
+  "use strict"
+
+  function Friends(props) {
+    return (
+      <div>
+        <p>Friends interesting facts version 1</p>
+      </div>
+    );
+  }
+
+  ReactDOM.render(<Friends />, document.getElementById("react-root"));
+})();
