24de5fdd8dcd304d1ee9c941de99c92044d173b6
Rennan Melo <rennan.melo11@gmail.com>
2021-05-28 05:59:35 -0300

    Created data file

---
+++ data.js
@@ -0,0 +1,83 @@
+window.FriendsData = {
+  'characters': [
+    { 
+      'name': 'Chandler Bing',
+      'lastname': 'Bing',
+      'gender': 'male',
+    },
+    { 
+      'name': 'Monica',
+      'lastname': 'Geller',
+      'gender': 'female',
+    },
+    { 
+      'name': 'Ross',
+      'lastname': 'Geller',
+      'gender': 'male',
+    },
+    { 
+      'name': 'Phoebe',
+      'lastname': 'Buffay',
+      'gender': 'female',
+    },
+    { 
+      'name': 'Rachel',
+      'lastname': 'Greene',
+      'gender': 'female',
+    },
+    { 
+      'name': 'Joey',
+      'lastname': 'Tribbiani',
+      'gender': 'male',
+    },
+    { 
+      'name': 'Richard',
+      'lastname': 'Burke',
+      'gender': 'male',
+    },
+  ],
+  
+  'seasons': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+  
+  byCharacter: {
+    'Chandler': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    'Monica': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    'Ross': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    'Phoebe': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    'Rachel': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    'Joey': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    'Richard': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+  },
+  bySeason: {
+    '1': [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey'
+    ],
+    '2':  [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey', 'Richard'
+    ],
+    '3':  [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey', 'Richard'
+    ],
+    '4':  [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey'
+    ],
+    '5':  [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey', 'Richard'
+    ],
+    '6':  [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey', 'Richard'
+    ],
+    '7':  [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey'
+    ],
+    '8':  [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey'
+    ],
+    '9':  [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey', 'Richard'
+    ],
+    '10':  [
+      'Chandler', 'Monica', 'Ross', 'Phoebe', 'Rachel', 'Joey'
+    ],
+  }
+}
