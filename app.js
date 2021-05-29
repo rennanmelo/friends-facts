c4d9fd53c6019a26e2d8aeb7abb572d99c1fa81e
Rennan Melo <rennan.melo11@gmail.com>
2021-05-29 07:02:18 -0300

    Created Character component

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 (function() {
   "use strict"
 
+  function Character(props) {
+    let character = props.character.name.toLowerCase();
+
+    return (
+      <img src={`/assets/${character}.jpg`} alt={`${props.character.name} ${props.character.lastname}`} />
+    );
+  }
+
   function Friends(props) {
+    let [character, setCharacter] = React.useState(window.FriendsData.characters[0]); 
+
     return (
       <div>
-        <p>Friends interesting facts version 1</p>
+        <Character character={character} />
       </div>
     );
   }
