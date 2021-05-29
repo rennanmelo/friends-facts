ac6e54cf53ca1d059eb4dc90ba5b02ee5c1e77b7
Rennan Melo <rennan.melo11@gmail.com>
2021-05-29 07:29:35 -0300

    Created CharacterSelect component

--- app.js
+++ app.js
@@ -9,12 +9,53 @@
     );
   }
 
+  function CharacterSelect(props) {
+    function characterOptions() {
+      return props.characters.map(char => {
+        return <option value={char.name} key={char.name}>{char.name}</option>
+      })
+    }
+
+    function onCharacterChange(event) {
+      props.handleCharacterChange(event.target.value);
+    }
+
+    return (
+      <div className='field-group'>
+        <label htmlFor='character-options'>Character:</label>
+        <select
+          defaultValue={props.character}
+          name='character_options'
+          id='character-options'
+          onChange={onCharacterChange}
+        >
+          {characterOptions()}
+        </select>
+      </div>
+    );
+  }
+
   function Friends(props) {
-    let [character, setCharacter] = React.useState(window.FriendsData.characters[0]); 
+    let [character, setCharacter] = React.useState(window.FriendsData.characters[0]);
+    let [characters, setCharacters] = React.useState(window.FriendsData.characters);
+
+    function handleCharacterChange(selectedCharacter) {
+      let availableCharacters = window.FriendsData.characters;
+      let newSelectedCharacter = availableCharacters.find(char => {
+        return char.name === selectedCharacter;
+      });
+
+      setCharacter(newSelectedCharacter);
+    }
 
     return (
       <div>
         <Character character={character} />
+        <CharacterSelect 
+          characters={characters}
+          character={character}
+          handleCharacterChange={handleCharacterChange}
+        />
       </div>
     );
   }
